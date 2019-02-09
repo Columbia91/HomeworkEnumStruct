9659ccaf1d84dd27bf2a349051afb2dccb19c709
ADILET <alpha.z91@mail.ru>
2019-02-09 19:02:31 +0600

    Change_1

--- PracticeStructEnum/Program.cs
+++ PracticeStructEnum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,20 +11,56 @@ namespace PracticeStructEnum
     {
         static void Main(string[] args)
         {
-            //Console.Write("Введите количество работников: ");
-            //int employeeCount = int.Parse(Console.ReadLine());
-            //Employee[] employees = new Employee[employeeCount];
-
-            //Console.WriteLine("\n\nВведите данные сотрудников ");
-            //for (int i = 0; i < employees.Length; i++)
-            //{
-            //    string str = Console.ReadLine();
-            //}
+            int employeeCount = 0;
             
-            foreach (var item in Enum.GetNames(typeof(Vacancies)))
+            string[] posts = { "Начальник", "Менеджер", "Бухгалтер", "Клерк", "Торговый агент" };
+            List<int> eachPostCounts = new List<int>(5);
+
+            Console.Write("Введите количество сотрудников:\n\n");
+            eachPostCounts.Insert(0,1);
+            employeeCount += eachPostCounts[0];
+            for (int i = 1; i < posts.Length; i++)
             {
-                Console.WriteLine(item);
+                Console.Write($"{posts[i]}: ");
+                eachPostCounts.Add(int.Parse(Console.ReadLine()));
+                employeeCount += eachPostCounts[i];
+            }
+            
+            Employee[] employees = new Employee[employeeCount];
+
+            DataEntry(employees, posts, eachPostCounts);
+            WorkersEngagedAfterBoss(employees, employees[0].DateOfEngagement);
+            
+        }
+        public static void DataEntry(Employee[] employees, string[] posts, List<int> eachPostCounts)
+        {
+            int iterator = 0;
+            for (int i = 0; i < posts.Length; i++)
+            {
+                Console.Clear();
+                Console.WriteLine("Введите данные  сотрудника");
+                for (int j = 0; j < eachPostCounts[i]; j++)
+                {
+                    Console.Write($"\nДолжность:\t{posts[i]}\n"); employees[iterator].Post = (Positions)iterator;
+                    Console.Write("Полное имя:\t"); employees[iterator].Name = Console.ReadLine();
+                    Console.Write("Зарплата:\t"); employees[iterator].Salary = int.Parse(Console.ReadLine());
+                    Console.Write("Дата приема:\t"); employees[iterator].DateOfEngagement = DateTime.Parse(Console.ReadLine());
+                }
+                iterator++;
+                Console.WriteLine();
+            }
+        }
+        public static void WorkersEngagedAfterBoss(Employee[] employees, DateTime bossDateOfEngagement)
+        {
+            Console.Clear();
+            Array.Sort(employees, new SortByName());
+            for (int i = 0; i < employees.Length; i++)
+            {
+                if (employees[i].DateOfEngagement > bossDateOfEngagement)
+                    employees[i].Show();
+                Console.WriteLine("\n");
             }
         }
     }
 }
+//foreach (var item in Enum.GetNames(typeof(Positions)))
